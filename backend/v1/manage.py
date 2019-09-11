f64eaff550e0d23a60875def51817e7e15176fdc
mondcub <mondcub@gmail.com>
2019-09-11 16:31:10 +0700

    added backend server, 3rdparty server into docker-compose

--- backend/v1/manage.py
+++ backend/v1/manage.py
@@ -1,15 +1,16 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 import unittest
 from flask import Flask
 from app import create_app
-# app = Flask(__name__)
+app = Flask(__name__)
 # configure your app
 
-manager = Manager(create_app)
+manager = Manager(app)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
 
-# @app.route('/token')
-# def hello():
-#     return "hello"
+@app.route('/token')
+def hello():
+    return "hello"
 
 @manager.command
 def test():
