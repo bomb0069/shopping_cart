1727cb5cf12f2f3686c72e4b1775efee73c373a3
chill <chill@chills-Mac-mini.local>
2019-09-11 15:19:31 +0700

    added script e2e test

---
+++ frontend/v1/tests/e2e/sample.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer')
+
+const launchConfig = {
+  headless: process.env.NODE_ENV === 'development',
+  slowMo: 100,
+  defaultViewport: {
+    width: 1280,
+    height: 800
+  }
+}
+
+describe('As a user I can sign in', () => {
+  let page
+  let browser
+  beforeAll(async () => {
+    browser = await puppeteer.launch(launchConfig)
+    page = await browser.newPage()
+  }, 30000)
+
+  afterAll(async () => {
+    await page.close()
+    await browser.close()
+  })
+
+  it('should load without error', async done => {
+    await page.goto('http://localhost:3000')
+    done()
+  }, 30000)
+}
+)
