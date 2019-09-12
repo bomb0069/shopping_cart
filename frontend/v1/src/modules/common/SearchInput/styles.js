359bf8d35c495dfe66b58f6718b3b9ce4ac95a95
mondcub <mondcub@gmail.com>
2019-09-12 16:50:24 +0700

    fixed, file delete searchinput

---
+++ frontend/v1/src/modules/common/SearchInput/styles.js
@@ -0,0 +1,17 @@
+export default theme => ({
+    center: {
+      justifyContent: 'center',
+      alignItem: 'center',
+      display: 'flex'
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+  })
