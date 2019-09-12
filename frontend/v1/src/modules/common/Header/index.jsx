ed6c3f20fb3f43bc4002350e3c2aa542473e61bf
programmua2204 <programmua.a@gmail.com>
2019-09-12 11:58:32 +0700

    created, header component prototype

--- frontend/v1/src/modules/common/Header/index.jsx
+++ frontend/v1/src/modules/common/Header/index.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import {withStyles, Paper} from '@material-ui/core'
+import React, { useState } from 'react'
+import {withStyles, Paper, Grid} from '@material-ui/core'
 import styles from './styles'
 import ButtonWithIcon from '../ButtonWithIcon'
+import SelectWithDropdown from '../SelectWithDropdown'
+import SearchInput from '../searchInput'
 
-const Header = ({classes}) => {
-    return (
-        <div className={classes.root}>
-            <Paper>
-            <ButtonWithIcon amount={0}/>
-            </Paper>
-        </div>
-    )
+const Header = ({classes, amount}) => {
+  const [searchValue, setSearchValue] = useState('')
+  const handleChange = event => {
+    setSearchValue(event.target.value)
+  }
+  return (
+    <Paper className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={10}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <SelectWithDropdown/>
+            </Grid>
+            <Grid item xs={12}>
+              <SearchInput id={'search-field'} value={searchValue} handleChange={handleChange} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2} style={{justifyContent: 'flex-end'}} >
+          <ButtonWithIcon amount={amount ? amount : 0}/>
+        </Grid>
+      </Grid>
+    </Paper>
+  )
 }
 
 export default withStyles(styles)(Header)
