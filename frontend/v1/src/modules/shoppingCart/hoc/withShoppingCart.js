bdda2c68974541cd651d1de7379f5dbcfbd60b54
Kaniknan <b_kaniknan@hotmail.com>
2019-09-12 10:50:12 +0700

    finished, create hoc shopping cart, added axios package and already made a request example waiting for mock server.

--- frontend/v1/src/modules/shoppingCart/hoc/withShoppingCart.js
+++ frontend/v1/src/modules/shoppingCart/hoc/withShoppingCart.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const withShoppingCart = (Component, userShoppingCartApi) => {
+    var config = {
+        headers: {'Access-Control-Allow-Origin': '*'}
+    }
     return () => {
         const [data, setData] = useState([])
         useEffect(() => {
-            const data = userShoppingCartApi.get()
+            // const data = userShoppingCartApi.get()
 
-            setData(data)
+            // 
+            axios.get('https://google.com', config)
+              .then(function (response) {
+                console.log('response', response)
+                setData([{1:1}])
+              })
+              .catch(function (error) {
+                console.log('error', error)
+                setData([{2:2}])
+              })
+            //   .then(function () {
+            //     // always executed
+            //   });
         }, [])
 
         return <Component data={data} />
