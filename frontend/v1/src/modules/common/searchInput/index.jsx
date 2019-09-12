8befe3f8ea4b0d1b29dc040da5c5773f38e2ebc3
mondcub <mondcub@gmail.com>
2019-09-12 10:23:24 +0700

    created,component searchInput,created component but finish

---
+++ frontend/v1/src/modules/common/searchInput/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const searchInput = () => {
+    return (
+        <div>
+           <input type = 'text'></input>
+           <button/>
+        </div>
+    )
+}
+
+export default searchInput
