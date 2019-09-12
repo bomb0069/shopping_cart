23204d8e631cc3aa7adca42eb5a09cfaa2b1f3ce
mondcub <mondcub@gmail.com>
2019-09-12 11:07:42 +0700

    added,component searchinput,add style and props

--- frontend/v1/src/modules/common/searchInput/index.jsx
+++ frontend/v1/src/modules/common/searchInput/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import { 
+  TextField,
+  withStyles,
+  IconButton,
+  Grid
+} from '@material-ui/core'
+import style from './styles'
+import SearchIcon from '@material-ui/icons/Search'
 
-const searchInput = () => {
+const searchInput = ({
+  classes,
+  handleChange,
+  handleClick,
+  value,
+  id
+}) => {
     return (
-        <div>
-           <input type = 'text'></input>
-           <button/>
-        </div>
+      <Grid container>
+        <Grid item xs={9}>
+          <TextField
+            id={id}
+            label="Search field"
+            type="search"
+            fullWidth
+            className={classes.textField}
+            onChang={handleChange}
+            value={value}
+            margin="normal"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={3} className={classes.center}>
+          <IconButton 
+            color="primary" 
+            className={classes.button}
+            onClick={handleClick}
+          >
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
     )
 }
 
-export default searchInput
+export default withStyles(style)(searchInput)
